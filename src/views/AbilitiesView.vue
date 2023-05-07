230124b44ab0e0ec19329ccca7bd45bb8f648899
Adem <ademduran6888@gmail.com>
2023-05-07 19:44:39 +0200

    feat(views): adding abilities main page

---
+++ src/views/AbilitiesView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1>Les capacités</h1>
+    <p>C’est ici que vous pouvez ajouter, modifier ou supprimer des capacités.</p>
+    <TableComponent
+      :head="arrayHead"
+      :body="arrayBody"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        arrayHead: ['Nom', 'Type', 'Quantité'],
+        arrayBody: [
+          ['Camouflage', 'Esquive', '2'],
+          ['Bouclier', 'Protection', '100'],
+          ['Concentration', 'Critique', '3'],
+          ['Colère des Dieux', 'Rage', '100'],
+          ['Châtiment', 'Malédiction', '1'],
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+  }
+</style>
