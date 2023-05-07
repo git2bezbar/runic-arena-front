f88cc650ee809316ecdf64449ce42cfd93f543b7
Adem <ademduran6888@gmail.com>
2023-05-07 23:11:19 +0200

    feat(routes): adding skill upsert page

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import AbilityView from '../views/AbilityView.vue'
 import AbilityUpsert from '../views/AbilityUpsert.vue'
 import SkillsView from '../views/SkillsView.vue'
 import SkillView from '../views/SkillView.vue'
+import SkillUpsert from '../views/SkillUpsert.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -55,6 +56,11 @@ const router = createRouter({
       path: '/skills/:id',
       name: 'skill',
       component: SkillView
+    },
+    {
+      path: '/skills/:id/edit',
+      name: 'skill-edit',
+      component: SkillUpsert
     },
     {
       path: '/classes',
