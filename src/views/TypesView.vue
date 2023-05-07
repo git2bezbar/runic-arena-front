7ff64d14d48fdbb7de5cfcf763fb3b2a5485b507
Adem <ademduran6888@gmail.com>
2023-05-08 01:05:10 +0200

    feat(views): adding types main page

---
+++ src/views/TypesView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <h1>Les types</h1>
+    <p>C’est ici que vous pouvez ajouter, modifier ou supprimer des types.</p>
+    <TableComponent
+      :head="arrayHead"
+      :body="arrayBody"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        arrayHead: ['Type'],
+        arrayBody: [
+          ['Halo'],
+          ['Chaos'],
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    align-items: flex-start;
+  }
+</style>
